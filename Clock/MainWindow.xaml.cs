44bdf1aa7a087680275f4c2858eb4938242c96ca
Tim Popov <popov_ta@mail.ru>
2022-03-01 22:48:45 +0300

    Добавьте файлы проекта.

---
+++ Clock/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Clock
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TimeClass timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new TimeClass(currentTimeTextBlock, docladTimeTextBlock);
+        }
+
+        private void S_OnClick(object sender, RoutedEventArgs e)
+        {
+            timer.StartTimer();
+            s.Content = !timer.timeDocEnabled ? "Старт" : "Пауза";
+        }
+
+        private void St_OnClick(object sender, RoutedEventArgs e)
+        {
+            timer.ClearTime();
+        }
+
+    }
+}
---
+++ Clock/TimeClass.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace Clock
+{
+    class TimeClass
+    {
+        DispatcherTimer timer;
+        private TextBlock currentTextBlock;
+        private TextBlock docladTextBlocks;
+
+
+        public TimeClass(TextBlock currentTb, TextBlock docladTb)
+        {
+            this.currentTextBlock = currentTb;
+            this.docladTextBlocks = docladTb;
+            InitializeTimer();
+        }
+
+        void InitializeTimer()
+        {
+            timer = new DispatcherTimer
+            { Interval = new TimeSpan(seconds: 1, hours: 0, minutes: 0) };
+            timer.Tick += Timer_Tick;
+            docTime = new TimeSpan(0);
+
+            timer.Start();
+
+
+        }
+
+
+
+        public void StartTimer()
+        {
+            timeDocEnabled = !timeDocEnabled;
+        }
+
+
+        public void ClearTime()
+        {
+            docTime = new TimeSpan(0);
+        }
+
+        private TimeSpan docTime;
+        public bool timeDocEnabled = false;
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            currentTextBlock.Text = DateTime.Now.ToLongTimeString();
+            docladTextBlocks.Text = new DateTime(docTime.Ticks).ToLongTimeString();
+
+            if (timeDocEnabled)
+            {
+                docTime = docTime.Add(new TimeSpan(seconds: 1, hours: 0, minutes: 0));
+            }
+        }
+
+
+    }
+}
